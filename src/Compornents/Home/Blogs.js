5539b050b3ee300a2993582c885d95e69be1c5a7
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-07-30 03:25:32 +0600

    added editing

--- src/Compornents/Home/Blogs.js
+++ src/Compornents/Home/Blogs.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h1>hello blogs</h1>
+            <h1 className='text-2xl text-center m-10'>Comming Soon.......</h1>
         </div>
     );
 };
