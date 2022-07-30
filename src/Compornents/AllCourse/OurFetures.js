a65c3020d6fb307968f99cc9da41252fde94b866
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-07-30 13:50:29 +0600

    added fetures

---
+++ src/Compornents/AllCourse/OurFetures.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const OurFetures = () => {
+    return (
+        <div>
+            <div className='my-10'>
+                <h1 className='lg:text-5xl md:text-3xl text-2xl text-center font-bold'>Discover Your Needs With Us</h1>
+                <h1 className='lg:text-3xl text-2xl text-center '>Choose your topic from our vast library to get started</h1>
+            </div>
+            
+        </div>
+    );
+};
+
+export default OurFetures;
