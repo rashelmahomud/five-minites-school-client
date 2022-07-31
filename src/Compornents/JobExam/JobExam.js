49d45990a8dabbf6e47c671f83ddedbdfe70653f
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-08-01 01:34:57 +0600

    added job exma site setup and design

--- src/Compornents/JobExam/JobExam.js
+++ src/Compornents/JobExam/JobExam.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const JobExam = () => {
+const JobExam = ({ exam }) => {
+    const { img, name } = exam;
+
     return (
-        <div>
-            
+        <div class="card card-compact  bg-base-100 shadow-xl">
+            <figure><img src={img} alt="Shoes" /></figure>
+            <div class="card-body">
+                <h2 class="card-title justify-center">{name}</h2>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-outline btn-secondary">SEE DETAILS</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Compornents/JobExam/JobExams.js
+++ src/Compornents/JobExam/JobExams.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import JobExam from './JobExam';
 
 const JobExams = () => {
 
@@ -16,6 +17,11 @@ const JobExams = () => {
             <div className='text-center'>
                 <h1 className='text-4xl font-bold'>Job Exam Preparation</h1>
                 <h2 className='text-2xl mt-2'>Which job are you preparing for?</h2>
+            </div>
+            <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 lg:my-20 my-5 lg:mx-10 mx-2 gap-5'>
+                {
+                    exams.map(exam => <JobExam exam={exam} key={exam._id}></JobExam>)
+                }
             </div>
         </div>
     );
