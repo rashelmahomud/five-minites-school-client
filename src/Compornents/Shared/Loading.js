14640a0fbe39537674df80d096ed4322d4a5c463
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-08-15 01:37:05 +0600

    added spiners

--- src/Compornents/Shared/Loading.js
+++ src/Compornents/Shared/Loading.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { ClimbingBoxLoader } from 'react-spinners';
 
 const Loading = () => {
     return (
         <div>
             <h1 className='text-3xl font-bold'>Five Minite School</h1>
             <div class="fixed top-0 right-0 h-screen w-screen z-50 flex justify-center items-center">
-                
 
-                <div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-4 border-orange-900">
 
-                </div>
+                <ClimbingBoxLoader
+                    color="#b17c05"
+                    size={30}
+                    speedMultiplier={1}
+                />
 
             </div>
         </div>
