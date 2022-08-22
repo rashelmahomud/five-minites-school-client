d59229ff167d604438b69616d96890feac3f0dda
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-08-22 21:41:00 +0600

    added some code for chat systesm

--- src/Compornents/Chat/Chat.js
+++ src/Compornents/Chat/Chat.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 import io from 'socket.io-client';
 import { nanoid } from 'nanoid';
 
-const userName = nanoid(5);
+
 const socket = io.connect("http://localhost:5000");
+const userName = nanoid(1);
 
 const Chat = () => {
+    const [user, loading, error] = useAuthState(auth);
+
     const [message, setMessage] = useState("");
     const [chat, setChat] = useState([]);
+
 
     const sentChat = (e) => {
         e.preventDefault();
@@ -28,15 +34,22 @@ const Chat = () => {
             <h1 className='my-5 text-center font-bold text-4xl'> COMMINUCATIONS YOUR FRINDS</h1>
 
 
-            <div className='text-center relative '>
+            <div className='text-center relative'>
                 <div class="mockup-phone">
                     <div class="camera"></div>
                     <div class="display">
-                        <div class="artboard artboard-demo phone-1">
+                        <div class="artboard artboard-demo phone-1 ">
+                            <div class="avatar online absolute top-10 ml-40">
+                                <div class="w-8 h-8 rounded-full top-10 ">
+                                    <img src={user?.photoURL} />
+                                </div>
+                            </div>
+
                             {chat.map((payload, index) => {
-                                return <p key={index}>{payload.message} <span className='font-bold'>id: {payload.userName}</span></p>;
+                                return <p className='text-2xl scroll-auto' key={index}>{payload.message} <span className='font-bold bg-base-300 p-2 rounded'>user: {payload.userName}</span></p>;
 
                             })}
+
                             <form onSubmit={sentChat} className='text-center bottom-0 fixed bottom-0 absolute mb-5 mt-2'>
 
                                 <input
